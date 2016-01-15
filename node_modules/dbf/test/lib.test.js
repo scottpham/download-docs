b7ebcff86165c183523958bbdf39eff4ce48fb7d
scottpham <scott.pham85@gmail.com>
2016-01-15 11:59:32 -0800

    got rid of node_modules

--- node_modules/dbf/test/lib.test.js
+++
@@ -1,8 +0,0 @@
-var test = require('tap').test,
-    lib = require('../src/lib');
-
-test('lib', function(t) {
-    t.equal(lib.rpad('test', 10, ' '), 'test      ', 'rpad');
-    t.equal(lib.lpad('test', 10, ' '), '      test', 'lpad');
-    t.end();
-});
--- node_modules/dbf/test/structure.test.js
+++
@@ -1,8 +0,0 @@
-var test = require('tap').test,
-    structure = require('../src/structure');
-
-test('structure', function(t) {
-    var dat = structure([{ foo: 'bar' }]);
-    t.ok(dat);
-    t.end();
-});
